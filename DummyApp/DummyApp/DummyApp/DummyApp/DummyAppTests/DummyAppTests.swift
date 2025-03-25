fcfa581c00b9f605c8b0911c037e0173842064ad
Dionicio <dioniciocruzvelazquez@gmail.com>
2025-03-25 17:16:46 -0400

    another ps

---
+++ DummyApp/DummyApp/DummyApp/DummyApp/DummyAppTests/DummyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DummyAppTests.swift
+//  DummyAppTests
+//
+//  Created by Dionicio Cruz Velázquez on 3/25/25.
+//
+
+import Testing
+@testable import DummyApp
+
+struct DummyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
